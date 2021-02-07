a07a6f11046b2de00cd5502f4acd74de277e0e8a
jakepd16 <78668653+jakepd16@users.noreply.github.com>
2021-02-06 21:52:30 -0500

    Add files via upload
    doen!

---
+++ assignment1/myfunc.cpp
@@ -0,0 +1,12 @@
+//myfunc.cpp
+//#includes are ALWAYS at top of file
+//iostream is where std::cout and std::endl are defined
+#include "myfunc.h"
+
+std::string func()
+{
+	//say hello
+	return "hello";
+}
+
+
